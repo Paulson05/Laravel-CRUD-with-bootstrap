f47d117572f14a19db52eee0bc1a30bd895d22d9
okofuonyebuchi@gmail.com <onyebuchiokofu@gmail.com>
2021-01-07 16:47:45 +0100

    create table created

--- product/resources/views/product/index.blade.php
+++ product/resources/views/product/index.blade.php
@@ -1,6 +1,7 @@
 @extends('product.layout')
 
 @section('content')
+<br><br><br>
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
@@ -11,6 +12,42 @@
                     </a>
             </div>
         </div>
+        <table class="table table-bordered table-responsive-lg">
+        <tr>
+            <th>Product name</th>
+            <th>Product code</th>
+            <th>Details</th>
+            <th>Product logo</th>
+            <th>Actions</th>
+        </tr>
+
+            <tr>
+                <td>shit</td>
+                <td>3424765</td>
+                <td>yugyugyugygfygyhgyggit</td>
+                <td></td>
+                <td>
+                    <form action="" method="POST">
+
+                        <a href="" title="show">
+                            <i class="btn btn-danger">SHOW</i>
+                        </a>
+
+                        <a href="">
+                            <i class="btn btn-primary">EDIT</i>
+                        </a>
+                        <a href="">
+                            <i class="btn btn-info">DELETE</i>
+                        </a>
+
+                      
+                    </form>
+                </td>
+            </tr>
+    
+    </table>
+
+
     </div>
 
    
